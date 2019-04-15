d172eaaaf3efcd0cdbeb7f949ffac1f560a908b5
Hanna Engel <hengel@alumni.berklee.edu>
2019-04-15 23:30:29 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,11 +2,9 @@ def my_each(array)
   i = 0
   newArray= []
   while i<array.length
-    yield
+    yield(array[i])
     newArray<<i
-    i+1
+    i = i+1
   end
   newArray
 end
-
-my_each(array){array|i| puts array[i]}
